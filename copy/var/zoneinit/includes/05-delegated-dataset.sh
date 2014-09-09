edca106309038b623f5d8130a106ccb5b251ec6b
glatze <ernst.glatz@hs-weingarten.de>
2014-09-09 15:39:07 +0200

    fixed user priv errors for /var/munin

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -5,4 +5,5 @@ DDS=zones/${UUID}/data
 if zfs list ${DDS} 1>/dev/null 2>&1; then
 	zfs create ${DDS}/munin   || true
 	zfs set mountpoint=/var/munin ${DDS}/munin
+	chown -R munin:munin /var/munin || true
 fi
