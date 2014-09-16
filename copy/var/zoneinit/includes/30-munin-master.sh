66d0cafa15308be5ff4fc33768c5dd4233327287
glatze <ernst.glatz@hs-weingarten.de>
2014-09-16 17:59:54 +0200

    fixed error: now using hostname instead of mdata-get because mdata-get fails if no hostname is set in mdata

--- copy/var/zoneinit/includes/30-munin-master.sh
+++ copy/var/zoneinit/includes/30-munin-master.sh
@@ -4,5 +4,5 @@ CRON="0,5,10,15,20,25,30,35,40,45,50,55 * * * * (sudo -u munin /opt/local/bin/mu
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
 ## Create localhost config for munin
-HOSTNAME=$(mdata-get sdc:hostname)
+HOSTNAME=$(hostname)
 echo -e "[${HOSTNAME}]\n    address 127.0.0.1\n    use_node_name yes\n" > /opt/local/etc/munin/munin-conf.d/localhost.conf
