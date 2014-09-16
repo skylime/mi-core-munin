f5a34473fce99abbc8b5c950fca0b05a7aea5452
glatze <ernst.glatz@hs-weingarten.de>
2014-09-16 16:31:13 +0200

    integrated munin_master_admin (django) into core-monitor

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -4,7 +4,12 @@ DDS=zones/${UUID}/data
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
 	zfs create ${DDS}/munin   || true
+	zfs create ${DDS}/munin-conf.d || true
+	zfs create ${DDS}/munin_master_admin_db || true
 	zfs set mountpoint=/var/munin ${DDS}/munin
+        zfs set mountpoint=/opt/local/etc/munin/munin-conf.d ${DDS}/munin-conf.d
+        zfs set mountpoint=/var/munin_master_admin_db ${DDS}/munin_master_admin_db
 	mkdir -p /var/munin/run
 	chown -R munin:munin /var/munin || true
+	chown -R www:www /var/munin_master_admin_db || true
 fi
