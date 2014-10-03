a43c24128f4a069a7ed8ad14fd8d513ff1e5fbe4
Thomas Merkel <tm@core.io>
2014-10-03 13:07:36 +0200

    new: usr: create extra user for django / nagiosadmin login

--- copy/var/zoneinit/includes/40-munin-master-admin.sh
+++ copy/var/zoneinit/includes/40-munin-master-admin.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 SETTINGS_FILE="/opt/munin_master_admin/munin_master_admin/settings.py"
+NAGIOS_ADMIN=${NAGIOS_ADMIN:-$(mdata-get nagios_admin 2>/dev/null)} || \
+NAGIOS_ADMIN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+mdata-put nagios_admin ${NAGIOS_ADMIN}
 
 /opt/local/bin/python -c "from django.utils.crypto import get_random_string;print 'SECRET_KEY = r\"' + get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%^&*(-_=+)') + '\"'" >> ${SETTINGS_FILE}
 
@@ -9,6 +12,10 @@ echo "DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': '/
 
 # Init django data and create admin user
 (sudo -u www /opt/munin_master_admin/manage.py syncdb --noinput)
+
+# Create superadmin user
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${NAGIOS_ADMIN}')" \
+	| /opt/munin_master_admin/manage.py shell
 
 ## Create cronjobs for munin_master_admin write_munin_config.py
 CRON="4,9,14,19,24,29,34,39,44,49,54,59 * * * * (/opt/munin_master_admin/manage.py write_munin_config)"
