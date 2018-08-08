9860695644a71a45ce849b2e33243da3f167e09e
Thomas Merkel <tm@core.io>
2018-08-08 11:55:38 +0200

    Use ssl-generate.sh from core-base

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# Enable nginx
 svcadm enable svc:/pkgsrc/nginx:default
+
+# Configure SSL
+/opt/core/bin/ssl-generate.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
+
+# Restart nginx to support new SSL certificate
+svcadm restart svc:/pkgsrc/nginx:default
