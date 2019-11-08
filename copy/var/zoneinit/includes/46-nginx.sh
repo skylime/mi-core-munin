22e20461173f3583475248eeb614c6a201da3539
Thomas Merkel <tm@core.io>
2019-11-08 11:39:19 +0100

    htaccess support and ssl-generator support for LE

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# Configure htaccess users from mdata variable
+if mdata-get nginx_auth >/dev/null 2>&1; then
+	for auth in $(mdata-get nginx_auth 2>/dev/null); do
+		echo ${auth} > /opt/local/etc/nginx/auth.htpasswd
+	done
+	gsed -i 's|#include auth.conf|include auth.conf|g' \
+		/opt/local/etc/nginx/nginx.conf
+fi
+
+
 # Enable nginx
 svcadm enable svc:/pkgsrc/nginx:default
 
 # Configure SSL
-/opt/core/bin/ssl-generate.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
+/opt/core/bin/ssl-generator.sh /opt/local/etc/nginx/ssl nginx_ssl nginx svc:/pkgsrc/nginx:default
 
 # Restart nginx to support new SSL certificate
 svcadm restart svc:/pkgsrc/nginx:default
