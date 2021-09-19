fb1bdb0a49b963a40c540fa6543f2f2aa9155263
Thomas Merkel <tm@core.io>
2021-09-19 14:02:08 +0200

    Add logadm for munin-master (and node) logfiles

--- copy/var/zoneinit/includes/30-munin-master.sh
+++ copy/var/zoneinit/includes/30-munin-master.sh
@@ -7,6 +7,10 @@ CRON="0,5,10,15,20,25,30,35,40,45,50,55 * * * * (sudo -u munin /opt/local/bin/mu
 HOSTNAME=$(hostname)
 echo -e "[${HOSTNAME}]\n    address 127.0.0.1\n    use_node_name yes\n" > /opt/local/etc/munin/munin-conf.d/localhost.conf
 
+## Add logadm to rotate munin-master logfiles by default
+logadm -r '/var/log/munin/munin-node.log'
+logadm -w '/var/log/munin/*.log' -A 14d -p 1d -c -N -m 640
+
 ## Enable FCGI for munin-graph and munin-html
 svcadm enable svc:/network/spawn-fcgi:munin-graph
 svcadm enable svc:/network/spawn-fcgi:munin-html
