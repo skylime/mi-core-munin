1b71c1d05613fed0a4a6dc9cc5b8868772ec6f52
Thomas Merkel <tm@core.io>
2025-09-27 23:53:06 +0200

    2024Q4 Release
    * New munstrap version with bootstrap 5 theme
    * New munin_master_admin version with latest django version and python
      3.12 support
    * Simple URL (_IRC_SECRET_TOKEN_) to send a information to irc via bot

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Configure htaccess users from mdata variable
-if mdata-get nginx_auth >/dev/null 2>&1; then
-	for auth in $(mdata-get nginx_auth 2>/dev/null); do
-		echo ${auth} > /opt/local/etc/nginx/auth.htpasswd
-	done
-	gsed -i 's|#include auth.conf|include auth.conf|g' \
-		/opt/local/etc/nginx/nginx.conf
+if mdata-get nginx_auth > /dev/null 2>&1; then
+    for auth in $(mdata-get nginx_auth 2> /dev/null); do
+        echo ${auth} > /opt/local/etc/nginx/auth.htpasswd
+    done
+    gsed -i 's|#include auth.conf|include auth.conf|g' \
+        /opt/local/etc/nginx/nginx.conf
 fi
-
+# Create secret for HTTP IRC Notification API
+IRC_SECRET_TOKEN=$(/opt/core/bin/mdata-create-password.sh -m irc_secret_token)
+gsed -i "s|_IRC_SECRET_TOKEN_|${IRC_SECRET_TOKEN}|g" \
+    /opt/local/etc/nginx/nginx.conf
 
 # Enable nginx
 svcadm enable svc:/pkgsrc/nginx:default
